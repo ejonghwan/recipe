d994d166a5fd7746aea24228b2145753f92e69dd
ejonghwan <sun87-1@daum.net>
2023-08-22 12:44:43 +0900

    레시피 데이터 출력

--- components/molecules/Category/Category.jsx
+++ components/molecules/Category/Category.jsx
@@ -2,14 +2,14 @@ import Btn from '@/components/atoms/Button/Btn';
 import styles from './Category.module.scss';
 import clsx from 'clsx';
 
-function Category({ items, setSelect }) {
-
-	// console.log()
-
+function Category({ items, onClick }) {
 	return (
 		<nav className={clsx(styles.category)}>
 			{items.map((el) => (
-				<Btn key={el.idCategory} onClick={() => setSelect(el.strCategory)}>{el.strCategory}</Btn>
+				//부모로부터 받은 onClick핸들러함수 props을 다시 Btn컴포넌트에 전달
+				<Btn key={el.idCategory} onClick={() => onClick(el.strCategory)}>
+					{el.strCategory}
+				</Btn>
 			))}
 		</nav>
 	);
--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -3,20 +3,18 @@ import styles from './style.module.scss';
 import axios from 'axios';
 import Category from '@/components/molecules/Category/Category';
 import { useRecipeByCategory } from '@/hooks/useRecipe';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Recipe({ categories }) {
-	// console.log(categories);
+	//react-query를 활용하는 쿼리키 인수값을 State에 담음
+	const [Selected, setSelected] = useState(categories[0].strCategory);
+	//해당 State값이 바뀔때마다 react-query훅이 호출되면서 새로운 데이터 패칭
+	const { data, isSuccess } = useRecipeByCategory(Selected);
+	//const obj = useRecipeByCategory(Selected);
+	//{data:서버데이타, isSuccess: boolean, refetch: func}
 
-	const [Select, setSelect] = useState('Beef');
-	const { data, isSuccess } = useRecipeByCategory(Select)
-	
-	
-	useEffect(() => {
-		console.log(Select)
-		console.log(data)
-	}, [Select, data])
-	
+	console.log(data);
+
 	return (
 		<>
 			<Head>
@@ -24,8 +22,19 @@ export default function Recipe({ categories }) {
 			</Head>
 
 			<section className={styles.recipePage}>
-				<Category items={categories} setSelect={setSelect}/>
-				{data?.map(item => <div key={item.idMeal}>{item.strMeal}</div>)}
+				{/* 이벤트가 발생해야 되는 자식요소에 queryKey로 연동되어 있는 state변경함수를 전달 */}
+				{/* 자식 컴포넌트에 이벤트 전달해야때 무조건 이벤트명 props 핸들러함수 전달 : 자식요소에 어떤이벤트에 어떤 핸들러가 보내지는 파악하기 위함 */}
+				{/* State변경하는 이벤트 핸들러함수를 onClick props에 담아서 전달 */}
+				<Category items={categories} onClick={setSelected} />
+				<div>
+					{data?.map((item, idx) => {
+						return (
+							<div key={idx}>
+								{item.strMeal}
+							</div>
+						)
+					})}
+				</div>
 			</section>
 		</>
 	);
