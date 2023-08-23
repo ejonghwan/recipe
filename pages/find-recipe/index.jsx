85e3439c98771844bd7445bc36a486fb5bf32f26
ejonghwan <sun87-1@daum.net>
2023-08-23 10:59:39 +0900

    카드 컴포넌트 스타일 적용

--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -5,6 +5,9 @@ import Category from '@/components/molecules/Category/Category';
 import { useRecipeByCategory } from '@/hooks/useRecipe';
 import { useState } from 'react';
 import { useDebounce } from '@/hooks/useDebounce';
+import Card from '@/components/molecules/Card/Card';
+import { Title } from '@/components/atoms/text/Title';
+import clsx from 'clsx';
 
 export default function Recipe({ categories }) {
 	const [Selected, setSelected] = useState(categories[0].strCategory);
@@ -13,8 +16,7 @@ export default function Recipe({ categories }) {
 	//useDebounce는 컴포넌트의 재랜더링 자체를 막는 것이 아닌
 	//특정 State가 변경될때마다 실행되는 무거운 함수의 호출 자체를 Debouncing하기 위함
 	const DebouncedSelected = useDebounce(Selected);
-	const { data, isSuccess } = useRecipeByCategory(DebouncedSelected);
-	console.log(data);
+	const { data: dataByCategory, isSuccess: isCategory } = useRecipeByCategory(DebouncedSelected);
 
 	return (
 		<>
@@ -24,6 +26,23 @@ export default function Recipe({ categories }) {
 
 			<section className={styles.recipePage}>
 				<Category items={categories} onClick={setSelected} />
+
+				<Title type={'slogan'} className={clsx(styles.titCategory)}>
+					{DebouncedSelected}
+				</Title>
+
+				<div className={clsx(styles.listFrame)}>
+					{isCategory &&
+						dataByCategory.map((el) => (
+							<Card
+								key={el.idMeal}
+								imgSrc={el.strMealThumb}
+								url={`/find-recipe/${el.idMeal}`}
+								txt={el.strMeal}
+								className={clsx(styles.card)}
+							/>
+						))}
+				</div>
 			</section>
 		</>
 	);
