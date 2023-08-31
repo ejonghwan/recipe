26d4c5e9a5856657f9dec56f7fc82b9c95c97365
ejonghwan <sun87-1@daum.net>
2023-08-31 12:42:45 +0900

    카테고리 추가 테마 변경 기능 추가

--- components/organisms/Footer/Footer.jsx
+++ components/organisms/Footer/Footer.jsx
@@ -1,12 +1,23 @@
-import { Title } from '@/components/atoms/text/Title';
 import styles from './Footer.module.scss';
 import clsx from 'clsx';
 import { Text } from '@/components/atoms/text/Text';
+import { BounceLoader } from 'react-spinners';
+import Btn from '@/components/atoms/Button/Btn';
+import { useGlobalData } from '@/hooks/useGlobalContext';
+import Category from '@/components/molecules/Category/Category';
 
 function Footer() {
+	const { setTheme, Theme } = useGlobalData();
 	return (
 		<footer className={clsx(styles.footer)}>
-			<Title style={{ fontSize: 16, color: '#777' }}>DCODELAB</Title>
+			<nav>
+				<Category
+					items={['theme1', 'theme2', 'theme3']}
+					names={['Orange', 'Aqua', 'Hot pink']}
+					active={Theme}
+					onClick={setTheme}
+				/>
+			</nav>
 			<Text type={'util'} style={{ letterSpacing: 2 }}>
 				2023 Decodelab All rights reserved.
 			</Text>
