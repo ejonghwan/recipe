dddde077daa134788ebb793f26bf274b95471b42
ejonghwan <sun87-1@daum.net>
2023-09-07 12:19:22 +0900

    배포테스트 8

--- components/atoms/form/InputCom.jsx
+++ components/atoms/form/InputCom.jsx
@@ -1,8 +1,8 @@
-import styles from './Input.module.scss';
+import styles from './InputCom.module.scss';
 import clsx from 'clsx';
 
-function Input({ type = 'text', placeholder = 'text', onChange, value, style, className }) {
+function InputCom({ type = 'text', placeholder = 'text', onChange, value, style, className }) {
 	return <input type={type} className={clsx(styles.input)} placeholder={placeholder} value={value} onChange={(e) => onChange(e.target.value)} style={style} />;
 }
 
-export default Input;
+export default InputCom;
